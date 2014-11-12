7b5725cbc5987b089300b680480420d39d4a3b8f
Graham Shaw <gdshaw@riscpkg.org>
2014-11-12 06:47:19 +0000

    Added class [cpudl].expr.Constant.

---
+++ src/org/codemancer/cpudl/expr/Constant.java
@@ -0,0 +1,34 @@
+// This file is part of Codemancer.
+// Copyright 2014 Graham Shaw.
+// Distribution and modification are permitted within the terms of the
+// GNU General Public License (version 3 or any later version).
+
+package org.codemancer.cpudl.expr;
+
+import org.codemancer.cpudl.type.Type;
+
+/** An expression class to represent a known integer constant. */
+public class Constant extends Expression {
+	/** The value of this integer constant. */
+	public final long value;
+
+	/** Construct integer constant from given value.
+	 * @param type the required type of this constant
+	 * @param value the required value
+	 */
+	public Constant(Type type, long value) {
+		super(type);
+		this.value = value;
+	}
+
+	/** Get value.
+	 * @return the value of this constant
+	 */
+	public long getValue() {
+		return value;
+	}
+
+	public String unparse() {
+		return new Long(value).toString();
+	}
+}
