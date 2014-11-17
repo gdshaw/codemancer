6f4c8d82f36ef8432513b0e0d0ef851990ad8453
Graham Shaw <gdshaw@riscpkg.org>
2014-11-17 20:37:18 +0000

    Added interface [cpudl].State and implementation [cpudl].EphemeralState.

---
+++ src/org/codemancer/cpudl/EphemeralState.java
@@ -0,0 +1,64 @@
+// This file is part of Codemancer.
+// Copyright 2014 Graham Shaw.
+// Distribution and modification are permitted within the terms of the
+// GNU General Public License (version 3 or any later version).
+
+package org.codemancer.cpudl;
+
+import java.io.PrintWriter;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.codemancer.cpudl.expr.Expression;
+import org.codemancer.cpudl.expr.Constant;
+import org.codemancer.cpudl.expr.Register;
+import org.codemancer.cpudl.expr.Memory;
+
+/** A class to represent the state of the machine at a single point in the program.
+ * Information about prior state is discarded once it has been superseded.
+ */
+public class EphemeralState implements State {
+	/** The current state of the registers. */
+	private TreeMap<String, Expression> registers = new TreeMap<String, Expression>();
+
+	/** The current state of memory. */
+	private TreeMap<Long, Expression> locations = new TreeMap<Long, Expression>();
+
+	public final Expression get(Register register) {
+		return registers.get(register.getName());
+	}
+
+	public final void put(Register register, Expression value) {
+		registers.put(register.getName(), value);
+	}
+
+	public final Expression get(Memory memory) {
+		Expression address = memory.getAddress().evaluate(this);
+		if (!(address instanceof Constant)) {
+			return null;
+		}
+		Constant constAddress = (Constant)address;
+		return locations.get(constAddress.getValue());
+	}
+
+	public final void put(Memory memory, Expression value) {
+		Expression address = memory.getAddress().evaluate(this);
+		if (!(address instanceof Constant)) {
+			return;
+		}
+		Constant constAddress = (Constant)address;
+		locations.put(constAddress.getValue(), value);
+	}
+
+	/** Dump the machine state to a given stream in human-readable form.
+	 * @param out the stream to which output should be sent
+	 */
+	public final void dump(PrintWriter out) {
+		for (Map.Entry<String, Expression> entry: registers.entrySet()) {
+			out.printf("%s: %s\n", entry.getKey(), entry.getValue().unparse());
+		}
+		for (Map.Entry<Long, Expression> entry: locations.entrySet()) {
+			out.printf("%08X: %s\n", entry.getKey(), entry.getValue().unparse());
+		}
+	}
+}
---
+++ src/org/codemancer/cpudl/State.java
@@ -0,0 +1,43 @@
+// This file is part of Codemancer.
+// Copyright 2014 Graham Shaw.
+// Distribution and modification are permitted within the terms of the
+// GNU General Public License (version 3 or any later version).
+
+package org.codemancer.cpudl;
+
+import org.codemancer.cpudl.expr.Expression;
+import org.codemancer.cpudl.expr.Register;
+import org.codemancer.cpudl.expr.Memory;
+
+/** An interface to represent the state of the machine.
+ * The purpose of this interface is to allow instructions to access and
+ * modify the machine state without regard to how and to what extent this
+ * state is recorded. It does not therefore provide any means to access
+ * prior state (however implementations are at liberty to provide such
+ * functionality if required).
+ */
+public interface State {
+	/** Get the value of a given register.
+	 * @param register the register to be inspected
+	 * @return the value of that register
+	 */
+	Expression get(Register register);
+
+	/** Put a value into a register.
+	 * @param register the register to be altered
+	 * @param value the value to be placed in that register
+	 */
+	void put(Register register, Expression value);
+
+	/** Get the value of a given memory location.
+	 * @param memory the memory location to be inspected
+	 * @return the value of that location
+	 */
+	Expression get(Memory memory);
+
+	/** Put a value into a memory location.
+	 * @param memory the memory location to be altered
+	 * @param value the value to be placed in that location
+	 */
+	void put(Memory memory, Expression value);
+}
